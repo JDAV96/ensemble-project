8b276d57a6a5ad50d62ff4ead3f35082e99ecb0a
Jose Arevalo <arevalojose2015@outlook.com>
2022-09-06 08:40:08 -0700

    search result button styling

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
+import Fab from '@mui/material/Fab';
 
 
 function ResultItem(props) {
-    return <div className="movie-result"><h1> {props.title} </h1> <p> {props.year} </p> <img src={props.poster} /> <button><DeleteIcon/></button></div>
+    return <div className="movie-result"><h1> {props.title} </h1> <p> {props.year} </p> <img src={props.poster} /> <Fab><p> PLACEHOLDER LABEL </p></Fab></div>
 }
 
 export default ResultItem;
