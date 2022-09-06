5727fab79c0322a21d0abf1685dadefd9ac62d03
Jose Arevalo <arevalojose2015@outlook.com>
2022-09-06 10:23:56 -0700

    grid for results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ResultItem from "./ResultItem";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function Results(props) {
-    return <div className="result-container">{props.searchResults.map((resultItem) => <ResultItem key={resultItem.imdbID} poster={resultItem.Poster} title={resultItem.Title} year={resultItem.Year}/>)}</div>
+    return <Box className="result-container">
+    <Grid container spacing={1}>
+    {props.searchResults.map((resultItem) => 
+    <Grid item xs={3}>
+    <ResultItem key={resultItem.imdbID} poster={resultItem.Poster} title={resultItem.Title} year={resultItem.Year}/>
+    </Grid>)}
+    </Grid>
+    </Box>
 }
 
 export default Results;
